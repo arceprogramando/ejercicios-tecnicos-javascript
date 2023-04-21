aa13b1849888484431c616e8526b600c087db564
Felipe Arce <arceprogramando@gmail.com>
2023-04-21 20:20:38 -0300

    subida ejercicio 20 trainee

---
+++ trainee/Ejercicio20/main.js
@@ -0,0 +1,16 @@
+let user = {
+    name: "John",
+    age: 30
+};
+
+
+
+const viewUser = (object) => {
+    console.log(object)
+    console.log(user.name)
+    console.log(user.age)
+
+
+}
+
+viewUser(user);
