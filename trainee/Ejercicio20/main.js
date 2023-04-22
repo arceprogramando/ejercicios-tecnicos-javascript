fc7a629c5a623f132130a4fb4760f73ee0e8b231
Felipe Arce <arceprogramando@gmail.com>
2023-04-21 22:40:41 -0300

    sincronizar

--- trainee/Ejercicio20/main.js
+++ trainee/Ejercicio20/main.js
@@ -1,5 +1,5 @@
 let user = {
-    name: "John",
+    nombres: "John",
     age: 30
 };
 
@@ -9,7 +9,7 @@ const viewUser = (object) => {
     console.log(object)
     console.log(user.name)
     console.log(user.age)
-
+    console.log("age" in user)
 
 }
 
