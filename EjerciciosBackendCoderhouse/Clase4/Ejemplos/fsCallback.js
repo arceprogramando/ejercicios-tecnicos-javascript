f1df9e51d9ccec90e4ed1982a3835c942731a74b
Felipe Arce <arceprogramando@gmail.com>
2023-05-03 18:11:28 -0300

    callback hell

--- EjerciciosBackendCoderhouse/Clase4/Ejemplos/fsCallback.js
+++ EjerciciosBackendCoderhouse/Clase4/Ejemplos/fsCallback.js
@@ -5,7 +5,7 @@ fs.writeFile('./ejemploCallback.txt', 'Hola desde Callback', (error) => {
         Salio mal durante la opreacion de escritura del archivo.
     */
     if (error) return console.log('Error al escribir el archivo') //Pregunto si el "error del callback existe."
-    fs.readFile('./ejemploCallback.txt'), 'utf-8', (error, resultado) => {
+    fs.readFile('./ejemploCallback.txt', 'utf-8', (error, resultado) => {
         /*
             Los mismos argumentos del readFileSync, solo que esta vez al final colocamos un callback , donde el primer
             argumento/parametro sirve para saber si hubo algun error al leer el archivo , el segundo argumento es el 
@@ -22,5 +22,7 @@ fs.writeFile('./ejemploCallback.txt', 'Hola desde Callback', (error) => {
                 if (error) return console.log('No se puede eliminar el archivo')
             })
         })
-    }
+    })
 })
+
+// Esto puede ser que entre en un CallbackHell
