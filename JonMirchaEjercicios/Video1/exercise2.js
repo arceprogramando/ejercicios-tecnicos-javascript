170d7ffc287522b29d00697037e57cd4f7a75b4d
Felipe Arce <arceprogramando@gmail.com>
2023-05-08 22:11:12 -0300

    sincronizacion

--- JonMirchaEjercicios/Video1/exercise2.js
+++ JonMirchaEjercicios/Video1/exercise2.js
@@ -1,3 +1,11 @@
 // Write a function that returns the text with the characters indicated in the parameter
 
 // Solution Jon mircha
+
+const recortarTexto = (cadena = "", longitud = undefined) => {
+    (!cadena)
+        ? console.error("No ingresaste una cadena de texto.")
+        : console.log((cadena.trim()).slice(0, longitud))
+}
+
+recortarTexto("Holas mundo", 5)
