2e52352394c123e3a0afb60cc3edb58ba8b0faf7
Felipe Arce <arceprogramando@gmail.com>
2023-05-10 22:49:41 -0300

    exercise3 jon mircha

--- JonMirchaEjercicios/Video1/exercise3.js
+++ JonMirchaEjercicios/Video1/exercise3.js
@@ -1,3 +1,15 @@
 /*Program a function that, given a string, returns an array of texts separated by a
  certain character, myFunction('Hello, what's up', '') will return ['Hello','what',''what's up']
  */
+
+const stringToArray = (textString = "", separator = undefined) => {
+    (!textString)
+        ? console.error("You did not enter a text string.")
+        : (separator === undefined)
+            ? console.error("You didn't enter the separator character")
+            : console.log(textString.split(separator))
+}
+
+stringToArray("Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus quos magni consequatur enim aliquid voluptates cupiditate, reiciendis cum numquam esse itaque maiores commodi quae nisi. Explicabo illo nam accusamus repellendus.")
+stringToArray("Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus quos magni consequatur enim aliquid voluptates cupiditate, reiciendis cum numquam esse itaque maiores commodi quae nisi. Explicabo illo nam accusamus repellendus.", ",")
+stringToArray("Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus quos magni consequatur enim aliquid voluptates cupiditate, reiciendis cum numquam esse itaque maiores commodi quae nisi. Explicabo illo nam accusamus repellendus.", " ")
