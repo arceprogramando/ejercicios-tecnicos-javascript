3da3a128cec9212d898c582a10e5f2af87094b8c
Felipe Arce <arceprogramando@gmail.com>
2023-05-14 22:30:40 -0300

    translate to english

--- trainee/Exercise11/main.js
+++ trainee/Exercise11/main.js
@@ -17,3 +17,10 @@ const squareSurface = (side) => {
 
 console.log(squareSurface(35))
 console.log(squareSurface("35"))
+
+/*To solve this exercise of giving the surface of the Square we create a function
+SquareSurface that will receive the value of the known side
+We are going to do a validation in which if side is Nan (Not a Number) it will use to return a throw new error
+in which it sends ("The side is not a number") this will be able to be handled in the future with try catch , we will see it later.
+we create our exercise in which we create our exercise in which according to mathematics the surface of a
+space is obtained by multiplying the sides, then we return surface */
